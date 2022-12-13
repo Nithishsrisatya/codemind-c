c1d950e0d3fbf3f86a71a8a84a3a46f70847165b
Nithishsrisatya <119840936+Nithishsrisatya@users.noreply.github.com>
2022-12-13 09:58:54 +0530

    #include<stdio.h> int main() {     int i,n;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     int min=a[0];     for(i=0;i<n;i++)     {         if(min<a[i])         min=a[i];     }     printf("%d",min); }

--- maximum_of_an_array.c
+++ maximum_of_an_array.c
@@ -8,11 +8,11 @@ int main()
     {
         scanf("%d",&a[i]);
     }
-    int max=a[0];
+    int min=a[0];
     for(i=0;i<n;i++)
     {
-        if (max<a[i])
-        max=a[i];
+        if(min<a[i])
+        min=a[i];
     }
-    printf("%d",max);
+    printf("%d",min);
 }
